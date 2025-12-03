16233ef9c94c3b480088476e0c0b6648575cc273
=Suci <=suciramadani2126@gmail.com>
2025-12-03 09:01:10 +0700

    upload file

---
+++ 2355201073_SuciRamadani/select_by_id.php
@@ -0,0 +1,27 @@
+<?php
+header("Content-Type: application/json; charset=UTF-8");
+
+if (!isset($_GET['id'])) {
+    http_response_code(400);
+    echo json_encode(["status" => "error", "msg" => "ID wajib dikirim"]);
+    exit();
+}
+
+$id = $_GET['id'];
+
+$k = new mysqli("localhost", "root", "", "db_mobil");
+
+$q = "SELECT * FROM mobil WHERE id='$id'";
+$res = $k->query($q);
+
+if ($res->num_rows === 0) {
+    http_response_code(404);
+    echo json_encode(["status" => "error", "msg" => "Data not found"]);
+    exit();
+}
+
+echo json_encode([
+    "status" => "success",
+    "msg" => "Process success",
+    "data" => $res->fetch_assoc()
+]);
