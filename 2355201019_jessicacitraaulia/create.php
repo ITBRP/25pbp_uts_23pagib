9f6547ddb914866105e8e8e9b4f714b18c130fde
=citra <=citraadumai@gmail.com>
2025-12-03 14:18:27 +0700

    selesai

--- 2355201019_jessicacitraaulia/create.php
+++ 2355201019_jessicacitraaulia/create.php
@@ -2,7 +2,7 @@
 header("Content-Type: application/json; charset=UTF-8");
 
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-    http_response_code(405);
+    http_response_code(500);
     echo json_encode(["status"=>"error","msg"=>"Server Error"]);
     exit();
 }
