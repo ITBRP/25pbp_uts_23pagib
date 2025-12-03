633fdd794d4c4574fecef0e1b477ff0cac44460a
=siti <=sitifatimahsaid3009@gmail.com>
2025-12-03 11:36:16 +0700

    selesai

--- 2355201080_sitifatimah/select.php
+++ 2355201080_sitifatimah/select.php
@@ -1,18 +1,38 @@
 <?php
 header("Content-Type: application/json; charset=UTF-8");
 
+// METHOD CHECK (GET ONLY)
+if ($_SERVER["REQUEST_METHOD"] !== "GET") {
+    http_response_code(500);
+    echo json_encode([
+        "status" => "error",
+        "msg" => "Server Error"
+    ]);
+    exit();
+}
+
+
+if (!isset($_GET['id'])) {
+    http_response_code(400);
+    echo json_encode(["status" => "error", "msg" => "ID wajib dikirim"]);
+    exit();
+}
+
+$id = $_GET['id'];
+
 $k = new mysqli("localhost", "root", "", "db_mobil");
 
-$q = "SELECT * FROM mobil ORDER BY id DESC";
+$q = "SELECT * FROM mobil WHERE id='$id'";
 $res = $k->query($q);
 
-$data = [];
-while ($row = $res->fetch_assoc()) {
-    $data[] = $row;
+if ($res->num_rows === 0) {
+    http_response_code(404);
+    echo json_encode(["status" => "error", "msg" => "Data not found"]);
+    exit();
 }
 
 echo json_encode([
     "status" => "success",
     "msg" => "Process success",
-    "data" => $data
+    "data" => $res->fetch_assoc()
 ]);
